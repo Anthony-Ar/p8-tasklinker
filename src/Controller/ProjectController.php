d903304b3b52576958cfeacb4f5f33175c91dcc8
Anthony <anthony.arrighi@orange.fr>
2025-04-08 13:39:12 +0200

    feat: Détail d'un projet

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Entity\Status;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,7 +18,22 @@ final class ProjectController extends AbstractController
     public function showProjects(): Response
     {
         return $this->render('pages/project/show.html.twig', [
-            'projects' => $this->entityManager->getRepository(Project::class)->findAll(),
+            'projects' => $this->entityManager->getRepository(Project::class)->findBy(['active' => true]),
+        ]);
+    }
+
+    #[Route('/project/{id}', name: 'app_show_project')]
+    public function showProject(int $id): Response
+    {
+        $project = $this->entityManager->getRepository(Project::class)->find($id);
+
+        if (!$project) {
+            return $this->redirectToRoute('app_show_projects');
+        }
+
+        return $this->render('pages/project/show_detail.html.twig', [
+            'status' => $this->entityManager->getRepository(Status::class)->findAll(),
+            'project' => $project
         ]);
     }
 }
