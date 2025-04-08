c9ea6bdc87fac8f7a4e16930b036f2b84ef31471
Anthony <anthony.arrighi@orange.fr>
2025-04-08 14:48:56 +0200

    feat: Modification d'une tâche existante

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -42,4 +42,31 @@ final class TaskController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    #[Route('/task/edit/{id}', name: 'app_edit_task')]
+    public function editTask(Request $request, int $id): Response
+    {
+        $task = $this->entityManager->getRepository(Task::class)->find($id);
+
+        if (!$task) {
+            return $this->redirectToRoute('app_show_projects');
+        }
+
+        $project = $task->getProject();
+
+        $form = $this->createForm(TaskType::class, $task, ['project' => $project]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($task);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_show_project', ['id' => $project->getId()]);
+        }
+
+        return $this->render('pages/task/edit.html.twig', [
+            'task' => $task,
+            'form' => $form->createView()
+        ]);
+    }
 }
